a0f695f149e8416e3f69301dc75889427201c7f0
=raghav <raghu112220gmail.com>
2019-05-26 05:13:48 +0530

    add new issue

---
+++ store/forms.py
@@ -0,0 +1,64 @@
+from django import forms
+from django.forms import (formset_factory, modelformset_factory)
+
+from .models import (Book, Author)
+
+
+class BookForm(forms.Form):
+    name = forms.CharField(
+        label='Book Name',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Book Name here'
+        })
+    )
+
+    myuser = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Book Name here'
+        })
+    )
+
+
+class BookModelForm(forms.ModelForm):
+
+    class Meta:
+        model = Book
+        fields = ('name', 'myuser' )
+        labels = {
+            'name': 'Book Name'
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Book Name here'
+                }
+            )
+        }
+
+
+BookFormset = formset_factory(BookForm)
+BookModelFormset = modelformset_factory(
+    Book,
+    fields=('name', ),
+    extra=1,
+    widgets={
+        'name': forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Book Name here'
+            }
+        )
+    }
+)
+
+AuthorFormset = modelformset_factory(
+    Author,
+    fields=('name', ),
+    extra=1,
+    widgets={'name': forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter Author Name here'
+        })
+    }
+)
